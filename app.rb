03367836575d5f765567c566194aea1bcee90923
ziesop20 <sincerelysophie@hotmail.com>
2018-07-31 19:40:49 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,6 +6,15 @@ class App < Sinatra::Base
     erb :food_form
   end
 
-  # Add your post route and action below
-
+# <form method="POST"action = "/food">
+#   <p> Your Name: <input type="text"></p>
+#   <input type="submit">
+# </form> 
+# params = {
+#   :name => "Sam",
+#   :favorite_food => "Green Eggs and Ham"
+# }
+post '/food' do
+ "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+end
 end
