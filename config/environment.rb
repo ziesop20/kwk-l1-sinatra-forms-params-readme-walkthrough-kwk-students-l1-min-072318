489e4956e47d4a629a63e3306d1e0ac9a2487886
ziesop20 <sincerelysophie@hotmail.com>
2018-07-31 19:31:41 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,8 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
+ params = {
+  :name => "Sam",
+  :favorite_food => "Green Eggs and Ham"
+}
 require './app'
